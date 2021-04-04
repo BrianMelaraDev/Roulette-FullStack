ba29a96f547792fcfb2796653659a9e3ee0b5868
Brian <brianmelaradev@gmail.com>
2021-04-03 22:16:06 -0400

    rouletteV1

---
+++ config/database.js
@@ -0,0 +1,12 @@
+// config/database.js
+module.exports = {
+  url: "mongodb+srv://brin:bananaclip@cluster0.afamw.mongodb.net/fullstack?retryWrites=true&w=majority",
+  dbName: "database",
+};
+
+/*
+heroku config:set MONGODB_URI="mongodb+srv://wadeMongo:wade2021@cluster0.uusdg.mongodb.net/moodTracker?retryWrites=true&w=majority"
+*/
+
+//admin@admin.com
+//password
